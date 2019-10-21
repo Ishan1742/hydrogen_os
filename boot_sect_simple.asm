37a4358f9d2b691b5d3c4332d4a9354cad5500fc
Ishan Ghosh <ishanghosh1999@gmail.com>
2019-10-21 14:32:08 +0530

    Print "hello world" upon boot

--- boot_sect_simple.asm
+++ boot_sect_simple.asm
@@ -1,9 +1,33 @@
-; Infinite loop
-loop:
-    jmp loop
+mov ah, 0x0e    ; tty mode (teletype/terminal)
+                ; tells the video iterrupt that 
+                ; the actual function we want to run is to 
+                ; "write the contents of al in tty mode"
+mov al, 'H'
+int 0x10        ; video interrupt
+mov al, 'e'
+int 0x10 
+mov al, 'l'
+int 0x10 
+int 0x10        ; 'l' is still on al 
+mov al, 'o'
+int 0x10 
+mov al, ' '
+int 0x10 
+mov al, 'W'
+int 0x10 
+mov al, 'o'
+int 0x10 
+mov al, 'r'
+int 0x10 
+mov al, 'l'
+int 0x10 
+mov al, 'd'
+int 0x10 
+mov al, '!'
+int 0x10
 
-; Fill with 510 zeros minus the size of the previous code
-times 510-($-$$) db 0
+jmp $           ; jump to the current address = infinite loop 
 
-; Magic number (makes the disk bootable)
+; padding and magic number
+times 510 - ($-$$) db 0
 dw 0xaa55
