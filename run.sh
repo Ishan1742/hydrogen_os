b03856956928ff2f25245250b6f2fa6d3bf5d321
Ishan Ghosh <ishanghosh1999@gmail.com>
2019-10-23 22:38:17 +0530

    Different ways to print memory element

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Compiling NASM"
-nasm -f bin boot_sect_simple.asm -o boot_sect_simple.bin 
+nasm -f bin boot_sect_memory.asm -o boot_sect_memory.bin 
 
 echo "Running file on emulator"
-qemu-system-x86_64 boot_sect_simple.bin
+qemu-system-x86_64 boot_sect_memory.bin
