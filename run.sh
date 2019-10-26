00fd61611eaa7107d9ca69c133d1bb9d123da219
Ishan Ghosh <ishanghosh1999@gmail.com>
2019-10-26 18:17:58 +0530

    Load data from disk to boot the kernel

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Compiling NASM"
-nasm -f bin boot_sect_segmentation.asm -o boot_sect_segmentation.bin
+nasm -f bin boot_sect_main.asm -o boot_sect_main.bin
 
 echo "Running file on emulator"
-qemu-system-x86_64 boot_sect_segmentation.bin
+qemu-system-x86_64 boot_sect_main.bin
