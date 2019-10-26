4c920ae92a9627376957b005e0f4f89ade7861bf
Ishan Ghosh <ishanghosh1999@gmail.com>
2019-10-26 17:59:10 +0530

    Address memory with segmentation

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 echo "Compiling NASM"
-nasm -f bin boot_sect_print.asm -o boot_sect_print.bin 
-nasm -f bin boot_sect_main.asm -o boot_sect_main.bin 
-nasm -f bin boot_sect_hex.asm -o boot_sect_hex.bin
+nasm -f bin boot_sect_segmentation.asm -o boot_sect_segmentation.bin
 
 echo "Running file on emulator"
-qemu-system-x86_64 boot_sect_main.bin
+qemu-system-x86_64 boot_sect_segmentation.bin
