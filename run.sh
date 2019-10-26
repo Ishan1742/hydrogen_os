f36bee984605b191638dac86bed7c3e361748f4a
Ishan Ghosh <ishanghosh1999@gmail.com>
2019-10-26 17:39:00 +0530

    Print hex values to check if correct value is read

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 echo "Compiling NASM"
 nasm -f bin boot_sect_print.asm -o boot_sect_print.bin 
 nasm -f bin boot_sect_main.asm -o boot_sect_main.bin 
+nasm -f bin boot_sect_hex.asm -o boot_sect_hex.bin
 
 echo "Running file on emulator"
 qemu-system-x86_64 boot_sect_main.bin
