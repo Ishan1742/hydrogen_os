89446397575affcd3d10142509e61f07ec7585ef
Ishan Ghosh <ishanghosh1999@gmail.com>
2019-10-29 15:44:45 +0530

    Write low level code in C

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "Compiling NASM"
-nasm -f bin 32bit-main.asm -o 32bit-main.bin
-
-echo "Running file on emulator"
-qemu-system-x86_64 32bit-main.bin
+gcc -march=i386 -fno-pie -m32 -ffreestanding -c function.c -o function.o
+objdump -M i386 -d function.o
+ld -melf_i386 -o function.bin -Ttext 0x0 --oformat binary function.o
+echo -e "\ncompiler output\n"
+xxd function.o
+echo -e "\nmachine output\n"
+xxd function.bin
+echo -e "\nmachine code\n"
+ndisasm -b 32 function.bin
